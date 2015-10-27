fc3cdef9c25f2c870801b8569e0a85ae8cf0a3ec
denovo <pete.richardson82@gmail.com>
2015-10-27 22:56:13 +0000

    adding react and jsx

---
+++ app/main.jsx
@@ -0,0 +1,6 @@
+console.log("this shit works!");
+var React = require('react/addons');
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+React.render(<GroceryItemList />, app);
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
 
 gulp.task('live-server', function() {
   var server = new LiveServer('server/main.js');
-  server.start()
+  server.start();
 })
 
-gulp.task('serve', ['live-server'], function() {
+gulp.task('bundle', function() {
+  return browserify({
+    entries: 'app/main.jsx',
+    debug: true,
+  })
+    .transform(reactify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./.tmp'));
+})
+
+gulp.task('serve', ['bundle', 'live-server'], function() {
   browserSync.init(null, {
     proxy: "http://localhost:7777",
     port: 9001
